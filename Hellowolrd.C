8ed054d9ee7b8548dd97fb7d8947b71674c8d29d
fabiane9258 <163157010+fabiane9258@users.noreply.github.com>
2024-03-23 00:58:20 +0300

    Add files via upload

---
+++ Hellowolrd.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+//this is random coversation ,don't know if you can tell the video it is from,hehe!
+    printf("Tony: What is your name?\n");
+    printf("Ezekiel: Ezekiel! What is your name?\n");
+    printf("Tony: Tony!\n");
+    printf("Ezekiel: I love coding.\n");
+    printf("Tony: Me tooo!\n");
+    printf("Ezekiel: Me toooo!\n\n");
+    
+    // Drake said this in his song "laugh now cry later"
+    printf("\"Sometimes we laugh and sometimes we cry but I guess you know now \" - Drake\n\n");
+
+    //ayoo! check this out 
+    printf("M\tO\tO\tD\nH\tO\tO\tD\nW\tO\tO\tD\t");
+    return 0;
+}
